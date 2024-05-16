9284e98510656081e59e7a91e567ea000676ce84
HASSAN KASSIM ELIAS <114056773+Hassankassim@users.noreply.github.com>
2024-05-16 18:30:47 +0300

    u

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,6 @@
 import TopicsList from "@/components/TopicsList";
 
 export default function Home() {
-  // return <TopicsList />;
-  return <div>Welcome on homepage</div>
+  return <TopicsList />;
+  // return <div>Welcome on homepage</div>
 }
